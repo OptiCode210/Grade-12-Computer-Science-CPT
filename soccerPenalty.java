1d302adcb9b1bb5470d369cdb9e717ae881c6e8b
OptiCode210 <210chrislau@gmail.com>
2026-05-20 11:28:03 -0400

    added paint compnent and drew ball, added variables

--- soccerPenalty.java
+++ soccerPenalty.java
@@ -4,16 +4,28 @@ import java.awt.event.*;
 import javax.swing.event.*;
 //For Paint Compnent
 
-public class soccerPenalty{
+public class soccerPenalty extends JPanel{
     //properties
-
+    int intBallX, intBallY;
 
 
     //methods
+    public void paintComponent(Graphics g){
+        g.setColor(Color.WHITE);
+        g.drawOval(intBallX, intBallY, 10, 10);
+    }
 
 
 
     //constructor
+    public soccerPenalty(){
+        super();
+
+        intBallX = 550;
+        intBallY = 650;
+
+        repaint();
+    }
 
 
 

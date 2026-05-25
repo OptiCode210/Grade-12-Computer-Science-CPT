c21993b3cd4bca0ae34b447447a28749fb138777
CassF26 <cassian.fung26@ycdsbk12.ca>
2026-05-25 19:55:11 -0400

    Update soccerMain.java

--- soccerMain.java
+++ soccerMain.java
@@ -7,9 +7,8 @@ import javax.imageio.*;
 import java.io.IOException;
 import java.io.File;
 
-public class soccerMain implements ActionListener{
+public class soccerMain extends JPanel implements ActionListener {
     //Properties
-    JPanel thePanel = new JPanel();
     JFrame theFrame = new JFrame("PENALTY!");
     
     //Main menu
@@ -27,11 +26,60 @@ public class soccerMain implements ActionListener{
 	String strServerID;
 	String strServerIP;
 	JComponent[] connectMenu;
+	boolean blnConnected = false;
 	SuperSocketMaster connectSSM = null;
 	
 	//Picking player
 	BufferedImage S1Front = null;
+	BufferedImage S1Run = null;
+	BufferedImage S1Shoot = null;
+	BufferedImage S1Stand = null;
 	
+	BufferedImage S2Front = null;
+	BufferedImage S2Run = null;
+	BufferedImage S2Shoot = null;
+	BufferedImage S2Stand = null;
+	
+	BufferedImage S3Front = null;
+	BufferedImage S3Run = null;
+	BufferedImage S3Shoot = null;
+	BufferedImage S3Stand = null;
+	
+	BufferedImage K1Left = null;
+	BufferedImage K1Right = null;
+	BufferedImage K1Stand = null;
+	
+	BufferedImage K2Left = null;
+	BufferedImage K2Right = null;
+	BufferedImage K2Stand = null;
+	
+	BufferedImage K3Left = null;
+	BufferedImage K3Right = null;
+	BufferedImage K3Stand = null;
+	
+	String strP1K;
+	String strP1S;
+	int intP1KAgi;
+	int intP1KCvg;
+	int intP1Pwr;
+	int intP1Acc;
+	
+	String strP2K;
+	String strP2S;
+	int intP2KAgi;
+	int intP2KCvg;
+	int intP2Pwr;
+	int intP2Acc;	
+
+	JButton confKButton = new JButton("Confirm");
+	JButton confSButton = new JButton("Confirm");
+	JLabel playerLabel = new JLabel("");
+	JLabel KAgiLabel = new JLabel("");
+	JLabel KCvgLabel = new JLabel("");
+	JLabel SPwrLabel = new JLabel("");
+	JLabel SAccLabel = new JLabel("");
+	JComponent[] selectionMenu;
+
     //Methods
     //For action listener
 	public void actionPerformed(ActionEvent evt){
@@ -47,18 +95,69 @@ public class soccerMain implements ActionListener{
 			System.out.println(connectSSM.getMyAddress());
 			strServerIP = connectSSM.getMyAddress();
 			connectSSM.connect();
+			System.out.println("Waiting for connection");
 		}
 		if(evt.getSource() == clientButton){
-			strServerID = JOptionPane.showInputDialog(theFrame, "Enter IP: ", "YIPPE", JOptionPane.PLAIN_MESSAGE);
+			strServerID = JOptionPane.showInputDialog(theFrame, "Enter IP: ", "Connect", JOptionPane.PLAIN_MESSAGE);
 			connectSSM = new SuperSocketMaster(strServerID, 6112, this);
+			strServerIP = connectSSM.getMyAddress();
 		
 			//Access the connect method
 			connectSSM.connect();
-			serverButton.setEnabled(false);
-			clientButton.setEnabled(false);
+			blnConnected = true;
 			System.out.println("CONNECTED");
 		}	
+		
+		//Going to player selection
+		if(blnConnected == true){
+			setMainVisible(false);
+			setConnectVisible(false);
+			setSelectionVisible(true);
+			thePanel.repaint();
+		}
 	}
+	
+	//Drawing the images
+	JPanel thePanel = new JPanel(){
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+			
+			if(blnConnected == true){				
+				if(S1Front != null){
+					g.drawImage(S1Front, 10, 100, 150, 200, null);
+				}
+		
+				if(blnConnected == true){				
+					if(S2Front != null){
+						g.drawImage(S2Front, 170, 100, 150, 200, null);
+					}
+				}
+				if(blnConnected == true){				
+					if(S3Front != null){
+						g.drawImage(S3Front, 330, 100, 150, 200, null);
+					}
+				}
+				if(blnConnected == true){				
+					if(K1Stand != null){
+						g.drawImage(K1Stand, 650, 100, 200, 200, null);
+					}
+				}
+				if(blnConnected == true){				
+					if(K2Stand != null){
+						g.drawImage(K2Stand, 860, 100, 200, 200, null);
+					}
+				}
+				if(blnConnected == true){				
+					if(K3Stand != null){
+						g.drawImage(K3Stand, 1070, 100, 200, 200, null);
+					}
+				}
+				
+				g.setColor(Color.BLACK);
+				g.fillRect(0, 378, 1280, 4);
+			}
+		}
+	};
 	
 	//Setting menus to visible or invisible
 	public void setMainVisible(boolean blnVisible){
@@ -73,13 +172,21 @@ public class soccerMain implements ActionListener{
 		}
 	}
 
+	public void setSelectionVisible(boolean blnVisible){
+		for(JComponent c:selectionMenu){
+			c.setVisible(blnVisible);
+		}
+	}
 
     //Constructor
     public soccerMain(){
         //Start window
+        super();
 		theFrame.setLayout(null);
 		theFrame.setPreferredSize(new Dimension(1280, 720)); 
-
+		thePanel.setLayout(null);
+		thePanel.setPreferredSize(new Dimension(1280, 720));
+		
 		//Main menu
 		playButton.setBounds(0,0,300,100);
 		playButton.addActionListener(this);
@@ -105,17 +212,48 @@ public class soccerMain implements ActionListener{
 		connectMenu = new JComponent[]{
 			serverButton, clientButton
 		};
+						
+		//Player selection images
+		try{
+			S1Front = ImageIO.read(new File("images/Strikers/S1Front.gif"));	
+			S1Run = ImageIO.read(new File("images/Strikers/S1Run.gif"));	
+			S1Shoot = ImageIO.read(new File("images/Strikers/S1Shoot.gif"));	
+			S1Stand = ImageIO.read(new File("images/Strikers/S1Stand.gif"));	
+			
+			S2Front = ImageIO.read(new File("images/Strikers/S2Front.gif"));	
+			S2Run = ImageIO.read(new File("images/Strikers/S2Run.gif"));	
+			S2Shoot = ImageIO.read(new File("images/Strikers/S2Shoot.gif"));	
+			S2Stand = ImageIO.read(new File("images/Strikers/S2Stand.gif"));
+			
+			S3Front = ImageIO.read(new File("images/Strikers/S3Front.gif"));	
+			S3Run = ImageIO.read(new File("images/Strikers/S3Run.gif"));	
+			S3Shoot = ImageIO.read(new File("images/Strikers/S3Shoot.gif"));	
+			S3Stand = ImageIO.read(new File("images/Strikers/S3Stand.gif"));
+			
+			K1Left = ImageIO.read(new File("images/keepers/K1Left.gif"));	
+			K1Right = ImageIO.read(new File("images/keepers/K1Right.gif"));	
+			K1Stand = ImageIO.read(new File("images/keepers/K1Stand.gif"));		
+			
+			K2Left = ImageIO.read(new File("images/keepers/K2Left.gif"));	
+			K2Right = ImageIO.read(new File("images/keepers/K2Right.gif"));	
+			K2Stand = ImageIO.read(new File("images/keepers/K2Stand.gif"));	
+			
+			K3Left = ImageIO.read(new File("images/keepers/K3Left.gif"));	
+			K3Right = ImageIO.read(new File("images/keepers/K3Right.gif"));	
+			K3Stand = ImageIO.read(new File("images/keepers/K3Stand.gif"));			
+		}catch(IOException e){
+			System.out.println("Image unable to be loaded");
+			System.out.println(e);
+		}
+		
+		selectionMenu = new JComponent[]{
+			confKButton, confSButton, playerLabel, KAgiLabel, KCvgLabel, SPwrLabel, SAccLabel
+		};
 		
 		//Drawing main screen
 		setMainVisible(true);
 		setConnectVisible(false);
-					
-		//Players
-		try{
-			S1Front = ImageIO.read(new File("S1Front.gif"));	
-		}catch(IOException e){
-			System.out.println("Image unable to be loaded");
-		}
+		setSelectionVisible(false);
 								
         //Finish window
         theFrame.setContentPane(thePanel);
@@ -128,6 +266,6 @@ public class soccerMain implements ActionListener{
 
     //Main method
 	public static void main(String[] args){			
-		
+		new soccerMain();
 	}
 }
